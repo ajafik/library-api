a10650c47e7e151c0e3f31514f53f64945ef1d8c
ajafik <oluwafikayoajayi@gmail.com>
2018-10-31 07:10:22 +0100

    Added express middlewares for request post data

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import * as express from "express";
-import * as bodyParser from "body-parser";
+import * as bodyParser from "body-parser"; //used to parse the form data that you pass in the request
 
 class App {
-    public app : express.Application;
+    public app: express.Application;
 
-    constructor(){
+    constructor() {
         this.app = express();
+        this.config();
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json()); // support application/json type post data
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({
+            extended: false
+        }));
+
     }
 
 }

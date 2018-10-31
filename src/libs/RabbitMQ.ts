68119d1c415b3dd5910f6c439f78d3989338786b
ajafik <oluwafikayoajayi@gmail.com>
2018-10-31 12:30:40 +0100

    Publishing and Consuming messages from RabbitMQ queue done

--- src/libs/RabbitMQ.ts
+++ src/libs/RabbitMQ.ts
@@ -2,6 +2,7 @@ import * as amqplib from 'amqplib';
 import { Config } from "../config/config";
 
 export class RabbitMQ {
+    client: any;
     RABBIT_HOST: any;
     RABBIT_PORT: number;
     RABBIT_USERNAME: string;
@@ -14,15 +15,50 @@ export class RabbitMQ {
         this.RABBIT_USERNAME = this.config.RabbitMQ().RABBIT_USERNAME;
         this.RABBIT_PASSWORD = this.config.RabbitMQ().RABBIT_PASSWORD;
 
+        this.client = amqplib.connect(`amqp://${this.RABBIT_USERNAME}:${this.RABBIT_PASSWORD}@${this.RABBIT_HOST}:${this.RABBIT_PORT}`);
+
+
     }
 
-    rabbit_send(QUEUE_NAME, message, timeout) {
+    rabbit_send(QUEUE_NAME, message) {
         return new Promise((resolve, reject) => {
-            amqplib.connect(`amqp://${this.RABBIT_USERNAME}:${this.RABBIT_PASSWORD}@${this.RABBIT_HOST}:${this.RABBIT_PORT}`).then(connection => {
-                console.log(connection);
-            }).catch(error => {
-                console.log(error);
-            })
+            this.client.then(function (connection) {
+                return connection.createChannel();
+            }).then(function (channel) {
+                return channel.assertQueue(QUEUE_NAME).then(function (res) {
+                    channel.sendToQueue(QUEUE_NAME, new Buffer(message));
+                    if (res.messageCount) {
+                        return resolve(true)
+                    } else {
+                        return reject(false)
+                    };
+                })
+            }).catch(console.warn);
+
         })
     }
+
+
+    rabbit_receive(QUEUE_NAME) {
+        return new Promise((resolve, reject) => {
+            this.client.then(function (connection) {
+                return connection.createChannel();
+            }).then(function (channel) {
+                return channel.assertQueue(QUEUE_NAME).then(function (res) {
+                    channel.consume(QUEUE_NAME, function (message) {
+                        if (message !== null) {
+                            resolve(message.content.toString())
+                            channel.ack(message);
+                        } else {
+                            reject("Unable to consume Message from the Queue.");
+                        }
+                    });
+                })
+            }).catch(console.warn);
+
+        })
+
+    }
+
+
 }

3e066d93b4510bfd12b0589260214c3d8b3928d9
ajafik <oluwafikayoajayi@gmail.com>
2018-10-31 10:08:24 +0100

    Added REDIS implementation

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export class Config {
     Redis(): any{
-        const PORT = process.env.REDIS_PORT || 1990;
+        const PORT = process.env.REDIS_PORT || 6379;
         const HOST = process.env.REDIS_HOST || "localhost"
 
         return {HOST,PORT};
--- src/libs/Redis.ts
+++ src/libs/Redis.ts
@@ -1,12 +1,82 @@
-import * as redis from 'redis';
-import { Config } from '../config/config';
+import * as redis from "redis";
+import * as bluebird from "bluebird";
 
-class Redis {
-    client: any;
-    config = new Config();
-    constructor() {
-        this.client = redis.createClient(this.config.Redis().PORT, this.config.Redis().HOST);
-    }
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+import { Config } from "../config/config";
+
+export class Redis {
+  client: any;
+  config = new Config();
+  constructor() {
+    this.client = redis.createClient(
+      this.config.Redis().PORT,
+      this.config.Redis().HOST
+    );
+  }
+
+  setString(key, value) {
+    return new Promise((resolve, reject) => {
+      this.client
+        .setAsync(key, value)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
+
+  getString(key) {
+    return new Promise((resolve, reject) => {
+      this.client
+        .getAsync(key)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
+
+  setObject(key, value) {
+    return new Promise((resolve, reject) => {
+      this.client
+        .hmsetAsync(key, value)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
+
+  getObject(key) {
+    return new Promise((resolve, reject) => {
+      this.client
+        .hgetallAsync(key)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
+
+  isKeyExist(key) {
+    return new Promise((resolve, reject) => {
+      this.client
+        .existsAsync(key)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
+
+  deleteKey(key) {
+    return new Promise((resolve, reject) => {
+      this.client
+        .delAsync(key)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
+
+  expireKey(key, expireTime) {
+    //expireTime in seconds
+    return new Promise((resolve, reject) => {
+      this.client
+        .expireAsync(key, expireTime)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  }
 }
-
-export default new Redis();

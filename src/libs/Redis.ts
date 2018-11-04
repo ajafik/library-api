4a07724ab749538e7bf753005d8f4a831020c5e0
ajafik <oluwafikayoajayi@gmail.com>
2018-11-04 18:21:15 +0100

    TSLint Redis module

--- src/libs/Redis.ts
+++ src/libs/Redis.ts
@@ -1,5 +1,5 @@
-import * as redis from "redis";
 import * as bluebird from "bluebird";
+import * as redis from "redis";
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
@@ -7,76 +7,76 @@ bluebird.promisifyAll(redis.Multi.prototype);
 import { Config } from "../config/config";
 
 export class Redis {
-  client: any;
-  config = new Config();
+  public client: any;
+  private config = new Config();
   constructor() {
     this.client = redis.createClient(
       this.config.Redis().PORT,
-      this.config.Redis().HOST
+      this.config.Redis().HOST,
     );
   }
 
-  setString(key, value) {
+  public setString(key, value) {
     return new Promise((resolve, reject) => {
       this.client
         .setAsync(key, value)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 
-  getString(key) {
+  public getString(key) {
     return new Promise((resolve, reject) => {
       this.client
         .getAsync(key)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 
-  setObject(key, value) {
+  public setObject(key, value) {
     return new Promise((resolve, reject) => {
       this.client
         .hmsetAsync(key, value)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 
-  getObject(key) {
+  public getObject(key) {
     return new Promise((resolve, reject) => {
       this.client
         .hgetallAsync(key)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 
-  isKeyExist(key) {
+  public isKeyExist(key) {
     return new Promise((resolve, reject) => {
       this.client
         .existsAsync(key)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 
-  deleteKey(key) {
+  public deleteKey(key) {
     return new Promise((resolve, reject) => {
       this.client
         .delAsync(key)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 
-  expireKey(key, expireTime) {
-    //expireTime in seconds
+  public expireKey(key, expireTime) {
+    // expireTime in seconds
     return new Promise((resolve, reject) => {
       this.client
         .expireAsync(key, expireTime)
-        .then(response => resolve(response))
-        .catch(error => reject(error));
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
   }
 }

6ddc81d74362045894c60d11ed210f4a4791df9e
ajafik <oluwafikayoajayi@gmail.com>
2018-11-04 18:04:33 +0100

    Lint App.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
+import * as bodyParser from "body-parser"; // used to parse the form data that you pass in the request
 import * as express from "express";
-import * as bodyParser from "body-parser"; //used to parse the form data that you pass in the request
-const API_VERSION = '/v1';
+const API_VERSION = "/v1";
 
-import {LibraryRoutes} from './routes/library';
+import {LibraryRoutes} from "./routes/library";
 
 class App {
     public app: express.Application;
@@ -18,9 +18,9 @@ class App {
 
     private config(): void {
         this.app.use(bodyParser.json()); // support application/json type post data
-        //support application/x-www-form-urlencoded post data
+        // support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({
-            extended: false
+            extended: false,
         }));
 
     }

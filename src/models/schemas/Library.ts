1d0637ef971d91a20808b7e9c162057df2b45cdd
ajafik <oluwafikayoajayi@gmail.com>
2018-11-06 07:04:46 +0100

    Added joigoose implementation for Library schema and added testing dependencies

--- src/models/schemas/Library.ts
+++ src/models/schemas/Library.ts
@@ -1,15 +1,20 @@
+import * as Joi from "joi";
+import * as Joigoose from "joigoose";
 import { Document, Model, model, Schema} from "mongoose";
+import * as Mongoose from "mongoose";
 import {ILibrary} from "../ILibrary";
+const joigoose = Joigoose(Mongoose);
 
 export interface ILibraryModel extends ILibrary, Document {}
 
-export let LibrarySchema: Schema = new Schema({
-    address: String,
-    createdAt: Date,
-    email: String,
-    library_name: String,
-    phoneNo: String,
-  });
+const joiLibrarySchema = Joi.object({
+    address: Joi.string().required(),
+    email: Joi.string().email().min(3).max(100).lowercase().required(),
+    library_name: Joi.string().min(3).max(100).required(),
+    phoneNo: Joi.string().min(5).max(15).required(),
+});
+
+const LibrarySchema = new Mongoose.Schema(joigoose.convert(joiLibrarySchema));
 
 LibrarySchema.pre("save", function(next) {
     const now = new Date();
